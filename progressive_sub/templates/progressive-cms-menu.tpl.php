86244316a836498f71e4a8748084eca12dc2bc96
Edwin Phillips <ed@kubuntu1604>
2017-08-28 20:04:40 +0100

    Remove elements from top header

--- progressive_sub/templates/progressive-cms-menu.tpl.php
+++ progressive_sub/templates/progressive-cms-menu.tpl.php
@@ -10,49 +10,6 @@ $one_page = isset($layout['settings']['one_page']) && $layout['settings']['one_p
         print render($block['content']);
         ?>
       </div>
-      <div class="col-xs-2 col-sm-2">
-      <div class="navbar navbar-inverse top-navbar top-navbar-right" role="navigation">
-        <button type="button" class="navbar-toggle btn-navbar collapsed" data-toggle="collapse" data-target=".top-navbar .navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <nav class="collapse collapsing navbar-collapse" style="width: auto;">
-          <ul class="nav navbar-nav navbar-right">
-            <?php if(theme_get_setting('account_login') && $user->uid): ?>
-              <li><?php print l(t('My Account'), 'user'); ?></li>
-            <?php endif; ?>
-            <?php if(module_exists('flag')):
-              $flags = flag_get_user_flags('node');
-              if(theme_get_setting('comparelist')): ?>
-                <li>
-                  <a href="<?php print url('product-compare'); ?>">
-                    <i class="fa fa-exchange"></i> <?php print t('Compare List'); ?><span class = "flag-counter flag-count-compare count"><?php print !isset($flags['compare']) ? 0 : count($flags['compare']); ?></span>
-                  </a>
-                </li>
-              <?php endif;
-              if(theme_get_setting('wishlist')): ?>
-                <li>
-                  <a href="<?php print url('shop/wishlist'); ?>">
-                    <i class="fa fa-heart"></i> <?php print t('My Wishlist'); ?><span class = "flag-counter flag-count-wishlist count"><?php print !isset($flags['wishlist']) ? 0 : count($flags['wishlist']); ?></span>
-                  </a>
-                </li>
-              <?php endif; ?>
-            <?php endif; ?>            
-            <?php if(theme_get_setting('cart_checkout') && module_exists('uc_cart')): ?>
-              <li><?php print l(t('My Cart <span class = "count cart-count">%number</span>', array('%number' => count(uc_cart_get_contents()))), 'cart', array('html' => TRUE)); ?></li>
-              <li><?php print l(t('Checkout'), 'cart/checkout'); ?></li>
-            <?php endif; ?>
-            <?php if(theme_get_setting('account_login') && !$user->uid): ?>
-              <li><?php print l(t('Log In  <i class="fa fa-lock after"></i>'), 'user/login', array('html' => TRUE, 'query' => array('destination' => $_GET['q']))); ?></li>
-            <?php endif; ?>
-            <?php if(theme_get_setting('account_login') && $user->uid): ?>
-              <li><?php print l(t('Log Out  <i class="fa fa-unlock after"></i>'), 'user/logout', array('html' => TRUE)); ?></li>
-            <?php endif; ?>
-          </ul>
-        </nav>
-      </div>
-      </div>
     </div>
     </div>
   </div>
@@ -80,16 +37,6 @@ $one_page = isset($layout['settings']['one_page']) && $layout['settings']['one_p
                 s2.687,6,6,6c1.402,0,2.688-0.484,3.71-1.29l0.79,0.79l-0.5,0.5l4,4l2-2L12.001,10z M6,10c-2.206,0-4-1.794-4-4s1.794-4,4-4
                 s4,1.794,4,4S8.206,10,6,10z"></path>
                 <!--<img src="<?php print base_path() . path_to_theme(); ?>/img/png-icons/search-icon.png" alt="" width="16" height="16" style="vertical-align: top;">-->
-              </svg>
-              </a>
-            </div>
-            <div class="phone-header hidden-600">
-              <a href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve">
-                <path d="M11.001,0H5C3.896,0,3,0.896,3,2c0,0.273,0,11.727,0,12c0,1.104,0.896,2,2,2h6c1.104,0,2-0.896,2-2
-                c0-0.273,0-11.727,0-12C13.001,0.896,12.105,0,11.001,0z M8,15c-0.552,0-1-0.447-1-1s0.448-1,1-1s1,0.447,1,1S8.553,15,8,15z
-                M11.001,12H5V2h6V12z"></path>
-                <!--<img src="<?php print base_path() . path_to_theme(); ?>/img/png-icons/phone-icon.png" alt="" width="16" height="16" style="vertical-align: top;">-->
               </svg>
               </a>
             </div>
